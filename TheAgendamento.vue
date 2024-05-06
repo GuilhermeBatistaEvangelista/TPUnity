e62c0108c512fffb26245b9cf413432e7d424647
Guilherme Batista Evangelista <72122919+GuilhermeBatistaEvangelista@users.noreply.github.com>
2024-05-06 16:56:39 -0300

    Update TheAgendamento.vue

--- TheAgendamento.vue
+++ TheAgendamento.vue
@@ -1,5 +1,5 @@
 <template>
-   <div class="agendamento">
+   <div class="agendamento bg-rose-100 dark:bg-zinc-400 dark:text-white">
     <p>Agendamentos aqui</p>
     <p>Agendamentos aqui</p>
     <p>Agendamentos aqui</p>
@@ -14,9 +14,13 @@
 .agendamento{
     width: 30%;
     height: 50%;
-    background-color: antiquewhite;
+    /*background-color: antiquewhite;*/
     position: absolute;
     top: 25%; left: 35%;
     align-content: center;
+    text-align: center;
+    border-width: 2px;
+    border-color: grey;
+    border-radius: 5%;
 }
 </style>

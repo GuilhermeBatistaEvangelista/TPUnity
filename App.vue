d0728f714e7c3820f87b8705821cf1aa4068ba43
Guilherme Batista Evangelista <72122919+GuilhermeBatistaEvangelista@users.noreply.github.com>
2024-05-07 17:20:20 -0300

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,9 +1,19 @@
 <template>
     <TheHeader />
-    <RouterView />
+    <div class="content bg-white text-black dark:bg-zinc-800 dark:text-white">
+        <RouterView />
+    </div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import TheHeader from './components/TheHeader.vue'
+
+
 </script>
+
+<style>
+.content{
+    height:90vh;
+}
+</style>

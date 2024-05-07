714f82996f6b6c5be7f4970bfc2062bb984d807a
Guilherme Batista Evangelista <72122919+GuilhermeBatistaEvangelista@users.noreply.github.com>
2024-05-07 17:21:04 -0300

    Update TheLogin.vue

--- TheLogin.vue
+++ TheLogin.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="login mx-auto">
-     <p>Nome: </p>
-     <input placeholder="Nome de Usuario">
-     <p>Senha:</p>
-     <Password v-model="value" placeholder="Senha"/>
-     <p class>
-        <RouterLink to="/dashboard"><button class="mx-10">Confirmar</button></RouterLink>
-     </p>
-  </div>
+   <div class="login mx-auto text-center">
+      <p>Nome: </p>
+      <input placeholder="Nome de Usuario">
+      <p>Senha:</p>
+      <Password class="h-2" v-model="value" :feedback="false" placeholder="Senha" toggleMask/>
+      <p class>
+         <RouterLink to="/dashboard"><button class="mx-10">Confirmar</button></RouterLink>
+      </p>
+   </div>
 </template>
 
 <script setup lang="ts">
@@ -19,29 +19,28 @@ const value = ref(null);
 
 <style scoped>
 .login {
-  place-content: center;
-  text-align: center;
-  width: 20%;
-  margin: auto;
-  background-color: aliceblue;
-  border-style: solid;
-  border-color: rgb(188, 217, 243);
-  border-width: 2px;
-  border-bottom-left-radius: 1%;
-  border-bottom-right-radius: 1%
+   place-content: center;
+   width: 20%;
+   margin: auto;
+   background-color: aliceblue;
+   border-style: solid;
+   border-color: rgb(188, 217, 243);
+   border-width: 2px;
+   border-bottom-left-radius: 1%;
+   border-bottom-right-radius: 1%
 }
 
 p {
-  color: black;
+   color: black;
 }
 
 button {
-  height: 80%;
-  width: 40%;
-  margin: auto;
-  background-color: aliceblue;
-  border-color: rgb(188, 217, 243);
-  margin-right: 4px;
-  border-radius: 10%;
+   height: 80%;
+   width: 40%;
+   margin: auto;
+   background-color: aliceblue;
+   border-color: rgb(188, 217, 243);
+   margin-right: 4px;
+   border-radius: 10%;
 }
 </style>

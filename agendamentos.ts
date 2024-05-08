a30aa9db70af02f4d6800343f4995e38425df24f
Guilherme Batista Evangelista <72122919+GuilhermeBatistaEvangelista@users.noreply.github.com>
2024-05-08 17:26:03 -0300

    Update agendamentos.ts

--- agendamentos.ts
+++ agendamentos.ts
@@ -1,21 +1,39 @@
-import http from "./api";
-//import { APIResponse } from "./types";
-type APIResponse<T> = {
-    success: boolean
-    content: T;
-    status?: number;
-}
+import api from "./api";
+import type { APIResponse } from "./types";
 
 type Agendamento = {
-    id: number;
-    name: string;
-    description: string;
-  };
+  id: number;
+  title: string;
+  url: string;
+};
 
 async function getAgendamentos() {
-    return await http.get<APIResponse<Agendamento[]>>("Agendamentos");
+  try {
+    const response = await api.get();
+    //const response = await api.get("1",{query:'url:"${data.url}"'});
+    //const response = await api.get<APIResponse<Agendamento>>('2');
+    /*const response: AxiosResponse<Agendamento[]> = await api.get("1");
+    const temp: Agendamento = response.data
+    console.log(temp);*/
+    console.log(response.data);
+    return response.data.slice(1,10)
+
+  } catch (error) {
+    console.error(error);
   }
-  
-  export default{
-    getAgendamentos,
-  };
+}
+async function getAgendamento(id: string) {
+  try {
+    const response = await api.get(id);
+    console.log(response);
+    return response.data
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default {
+  getAgendamentos,
+  getAgendamento,
+};
